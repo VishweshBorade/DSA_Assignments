81f3883e67951f2aec46176ff03d59ff9e9f3c74
Vishwesh Vikas Borade <95901342+VishweshBorade@users.noreply.github.com>
2024-05-24 11:32:14 +0530

    Create ArrayInter.js

---
+++ Assignment3/ArrayInter.js
@@ -0,0 +1,17 @@
+function inter(arr1, arr2) {
+  const s1 = new Set(arr1);
+
+  const intersectionSet = new Set();
+
+  for (const element of arr2) {
+    if (s1.has(element)) {
+      intersectionSet.add(element);
+    }
+  }
+
+  return Array.from(intersectionSet);
+}
+
+const array1 = [1, 2, 2, 1];
+const array2 = [2, 2];
+console.log(inter(array1, array2));
